69fddf67ac642ddd6526bfe44dd4f75bbb0d6940
Michał Kaczmarek <michal31kaczmarek@gmail.com>
2024-10-08 18:00:06 +0200

    Cumtribution

---
+++ lab2/zad2.py
@@ -0,0 +1,137 @@
+import numpy as np
+import matplotlib . pyplot as plt
+from scipy import signal
+from scipy.integrate import odeint
+
+def calman_matrix(A, B):
+    columns = [np.linalg.matrix_power(A, i) @ B for i in range(A.shape[1])]
+    result_matrix = np.column_stack(columns)
+    return result_matrix
+
+def getAs(A):
+    eigenvalues = np.linalg.eigvals(A)
+    polynomial_coeffs = np.poly(eigenvalues)
+    rows = []
+    for i in range(0, polynomial_coeffs.size-2):
+        new_vec = np.zeros(polynomial_coeffs.size-1)
+        new_vec[i+1] = 1
+        rows.append(new_vec)
+    rows.append(-polynomial_coeffs[1:][::-1])
+    return np.vstack(rows)
+
+def getBs(B):
+    Bs = np.zeros_like(B)
+    Bs[-1, 0] = 1
+    return Bs
+
+def getPinv(As, Bs, A, B):
+    return calman_matrix(A, B) @ np.linalg.inv(calman_matrix(As, Bs))
+
+def plot_all_state_vars_Pinv(A,B, Pinv, utype = 0):
+    D = 0
+    for i in range(A.shape[1]):
+        C = np.zeros(A.shape[1])
+        C[i] = 1
+        if Pinv is not None:
+            C = C @ Pinv
+        
+        SS = signal.StateSpace(A,B,C,D)
+        if utype == 0:
+            t, y = signal.step(SS)
+        elif utype == 1:
+            time = np.linspace(0, 40, 100)
+            u = np.ones_like(time) * 2
+            t, y, x_out = signal.lsim(SS, u, time)
+        else:
+            time = np.linspace(0, 40, 100)
+            u = [ (np.sin(x) if x > 0 else 0) - 1/2 for x in time]
+            t, y, x_out = signal.lsim(SS, u, time)
+        plt.plot(t,y, label=f"x{i+1}")
+    plt.legend()
+    plt.show()
+
+def zad21_rys2():
+    C1 = 1
+    R1 = 1
+    C2 = 2
+    R2 = 1
+    C3 = 3
+    R3 = 1
+
+    ##### State-equasions #####
+    A = np.array([[-1/(R1*C1), 0, 0],[0, -1/(R2*C2), 0],[0, 0, -1/(R3*C3)]])
+    B = np.array([[1/(R1*C1)],[1/(R2*C2)],[1/(R3*C3)]])
+
+    As = getAs(A)
+    Bs = getBs(B)
+    Pinv = getPinv(As, Bs, A, B)
+    plot_all_state_vars_Pinv(As, Bs, Pinv , utype = 0)
+    plot_all_state_vars_Pinv(A, B, None, utype = 0)
+    # jest sterowalny
+
+def zad21_rys4():
+    R1 = 2
+    L1 = 0.5
+    R2 = 1
+    L3 = 1
+    C4 = 2
+
+    ##### State-equasions #####
+    A = np.array([[-R1/L1, 0, -1/L1],[0, 0, 1/L3],[1/C4, -1/C4, -1/(R2*C4)]])
+    B = np.array([[1/L1],[0],[0]])
+    
+    As = getAs(A)
+    Bs = getBs(B)
+    Pinv = getPinv(As, Bs, A, B)
+    plot_all_state_vars_Pinv(As, Bs, Pinv , utype = 0)
+    plot_all_state_vars_Pinv(A, B, None, utype = 0)
+    # jest sterowalny
+
+def calman_theorem_rank(A, B):
+    columns = [np.linalg.matrix_power(A, i) @ B for i in range(A.shape[1])]
+    result_matrix = np.column_stack(columns)
+    print(f"Calman matrix: {result_matrix}")
+    return np.linalg.matrix_rank(result_matrix)
+
+def zad21_rys1():
+    C1 = 1
+    R1 = 2
+    C2 = 0.5
+    R2 = 4
+
+    ##### State-equasions #####
+    A = np.array([[-1/(R1*C1), 0],[0, -1/(R2*C2)]])
+    B = np.array([[1/(R1*C1)],[1/(R2*C2)]])
+    As = getAs(A)
+    Bs = getBs(B)
+    Pinv = getPinv(As, Bs, A, B)
+    plot_all_state_vars_Pinv(As, Bs, Pinv , utype = 0)
+    plot_all_state_vars_Pinv(A, B, None, utype = 0)
+    # jest niesterowalny
+
+def zad21_rys3():
+    R = 1
+    C1 = 0.1
+    L1 = 0.1
+    C2 = 0.1
+    L2 = 0.1
+
+    ##### State-equasions #####
+    A = np.array([[0, 1/L1, 0, 0],[-1/C1, -1/(R*C1), 0, -1/(R*C1)],[0, 0, 0, 1/L2], [0, -1/(R*C2), -1/C2, -1/(R*C2)]])
+    B = np.array([[0],[-1/(R*C1)],[0],[-1/(R*C2)]])
+    As = getAs(A)
+    Bs = getBs(B)
+    Pinv = getPinv(As, Bs, A, B)
+    plot_all_state_vars_Pinv(As, Bs, Pinv , utype = 0)
+    plot_all_state_vars_Pinv(A, B, None, utype = 0)
+    print(f"Rank before: {calman_theorem_rank(A, B)}")
+    print(f"Rank after: {calman_theorem_rank(As, Bs)}")
+    # nie jest sterowalny
+
+if __name__ == "__main__":
+    zad21_rys4()
+    # Dla układów niesterowalnych idzie wyznaczyć postać sterowalną. Wtedy stają się sterowalne
+    # Opisują obiekty w różny sposób ale efekt jest ten sam
+    # Przebiegi są takie same, to oznacza że do projektowania układu regulacji możemy użyć postaci sterowalnej równań stanu
+    
+   
---
+++ lab2/zad3.py
@@ -0,0 +1,91 @@
+from matplotlib.pylab import det
+import numpy as np
+import matplotlib . pyplot as plt
+from scipy import signal
+from scipy.integrate import odeint
+from sympy import Matrix, simplify, symbols
+
+def characteristic_equation(A, B, K):
+    s = symbols('s')
+    A_closed = A - B @ K
+    char_matrix = s * Matrix.eye(A.shape[0]) - A_closed
+    characteristic_eq = char_matrix.det()
+    return characteristic_eq
+
+# Dla sterowalnej postaci układu z Rys. 2 wyznaczyć równanie charakterystyczne układu
+# zamkniętego ze sprzężeniem od stanu (11). W tym celu można skorzystać z relacji (13).
+def zad31():
+    C1 = 1
+    R1 = 1
+    C2 = 2
+    R2 = 1
+    C3 = 3
+    R3 = 1
+
+    ##### State-equasions #####
+    k1, k2, k3 = symbols("k1 k2 k3")
+
+    A = np.array([[-1/(R1*C1), 0, 0],[0, -1/(R2*C2), 0],[0, 0, -1/(R3*C3)]])
+    B = np.array([[1/(R1*C1)],[1/(R2*C2)],[1/(R3*C3)]])
+    K = np.array([[k1, k2, k3]])
+    print(simplify(characteristic_equation(A, B, K)))
+    # k1*s**2 + 0.83333*k1*s + 0.16666*k1 + 0.5*k2*s**2 + 0.6666*k2*s + 0.1666*k2 + 0.3333*k3*s**2 + 0.5*k3*s + 0.16666*k3 + s**3 +1.83333*s**2 + s + 0.1666
+
+# Wyznaczyć takie wartości macierzy K, aby bieguny układu zamkniętego wynosiły odpowiednio
+# s1 = −1, s1 = −2, s3 = −5. Uwaga: idealne równanie charakterystyczne dla przyjętych 
+# biegunów wynosi s^3 + 8s^2 + 17s + 10
+def zad32():
+    desired_poles = [-1, -2, -5]
+    C1 = 1
+    R1 = 1
+    C2 = 2
+    R2 = 1
+    C3 = 3
+    R3 = 1
+    
+    A = np.array([[-1/(R1*C1), 0, 0],[0, -1/(R2*C2), 0],[0, 0, -1/(R3*C3)]])
+    B = np.array([[1/(R1*C1)],[1/(R2*C2)],[1/(R3*C3)]])
+
+    K = signal.place_poles(A, B, desired_poles).gain_matrix
+
+    print(f'K vector: {K}')
+
+# Przeprowadzić symulację odpowiedzi obiektu na wymuszenie (11).
+# • Jaki jest charakter odpowiedzi układu zamkniętego?
+# • Jak poszczególne wartości wzmocnień K wpływają na pozycje biegunów układu zamkniętego?
+def zad33():
+    desired_poles = [-1, -2, -5]
+    C1 = 1
+    R1 = 1
+    C2 = 2
+    R2 = 1
+    C3 = 3
+    R3 = 1
+    
+    A = np.array([[-1/(R1*C1), 0, 0],[0, -1/(R2*C2), 0],[0, 0, -1/(R3*C3)]])
+    B = np.array([[1/(R1*C1)],[1/(R2*C2)],[1/(R3*C3)]])
+    C = np.array([1, 2, 3]) # nie było podane jaki jest sygnał wyjściowy
+    D = 0
+    
+    K = np.array(signal.place_poles(A, B, desired_poles).gain_matrix)
+    K = np.array([[10, 0, 0]])
+    
+    current_A = (A - B @ K)
+    print(f"Bieguny nowej macierzy A: {np.linalg.eigvals(current_A)}") # Bieguny teraz są dokładnie w tym miejscu co chcieliśmy żeby były
+    current_C = (C - D * K)
+
+    SS_before = signal.StateSpace(A, B, C, D)
+    SS_after = signal.StateSpace(current_A, B, current_C, 0)
+
+    t,y = signal.step(SS_before)
+    plt.plot(t,y)
+    plt.show()
+    t,y = signal.step(SS_after)
+    plt.plot(t,y)
+    plt.show()
+    # • Odpowiedź układu się stabilizuje na 0.1. Dynamika jest większa, ale pojawia się przeregulowanie (coś za coś).
+    # • Wpływają zgodnie z równaniem wyzmaczonym w zadaniu 3.1 (miejsca zerowe wielomianu to miejsca biegunów)
+
+if __name__ == "__main__":
+    zad31()    
+   
