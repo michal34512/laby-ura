f70d493cdd324ccc8bb37d305931a375f181e016
Michał Kaczmarek <michal31kaczmarek@gmail.com>
2024-11-22 10:50:08 +0100

    wskaznik OPT

--- lab4/zad4.py
+++ lab4/zad4.py
@@ -20,10 +20,6 @@ def plot_PID(nastawy):
     L2 = 0.5
     yd = 1
     (kp, ki, kd) = nastawy
-    kp = 1000
-    ki = 34.7
-    kd = 194.4
-
     def model(xe, t): # e_dot = -y_dot
         x1, x2, x3, e_int, I_ISE, I_ITSE, I_IAE, I_ITAE  = xe
         # PID
@@ -89,14 +85,14 @@ def simulate_PID(kp, ki, kd):
     x0 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
     sol = odeint(model, x0, t)
-    i_ise = sol[:, 7]  # I_ISE
+    i_ise = sol[:, 4]  # I_ISE
 
     return i_ise[-1]
 
 def optimize_PID():
-    kp_values = np.linspace(10000, 20000, 10)  # Możliwe wartości kp
-    ki_values = np.linspace(100, 500, 10)  # Możliwe wartości ki
-    kd_values = np.linspace(100, 800, 10)  # Możliwe wartości kd
+    kp_values = np.linspace(100, 100, 1)  # Możliwe wartości kp
+    ki_values = np.linspace(19, 24, 20)  # Możliwe wartości ki
+    kd_values = np.linspace(23, 28, 20)  # Możliwe wartości kd
     
     best_kp, best_ki, best_kd = 0, 0, 0
     best_ISE = float('inf')
@@ -116,6 +112,58 @@ def optimize_PID():
 
     return best_kp, best_ki, best_kd, best_ISE
 
+def simulate_PID_opt(nastawy, q, r):
+    R1 = 2
+    R2 = 5
+    C1 = 0.5
+    L1 = 2
+    L2 = 0.5
+    yd = 1
+    (kp, ki, kd) = nastawy
+    def model(xe, t): # e_dot = -y_dot
+        x1, x2, x3, e_int, I_OPT  = xe
+        # PID
+        e_dot = -(x2*(-R2/L2) + x3 * (1/L2))
+        e = yd - x2
+        u = kp * e + ki * e_int + kd * e_dot
+        OPT = q * e**2 + r * u**2
+
+
+        x1_dot = x1*(-R1/L1) + x3 * (-1/L1) + u * (1/L1)
+        x2_dot = x2*(-R2/L2) + x3 * (1/L2)
+        x3_dot = x1*(1/C1) + x2 * (-1/C1)
+
+        return [x1_dot, x2_dot, x3_dot, e, OPT]
+    t = np.linspace(0, 10, 50000)
+    x0 = np.array([0.0, 0.0, 0.0, 0.0, 0.0])
+    sol = odeint(model, x0, t)
+    x1, x2, x3, e_int, i_opt = sol.T
+    return i_opt[-1]
+
+def optimize_PID_opt(q, r):
+    kp_values = np.linspace(5, 12, 10)  # Możliwe wartości kp
+    ki_values = np.linspace(0, 2, 20)  # Możliwe wartości ki
+    kd_values = np.linspace(4, 10, 20)  # Możliwe wartości kd
+    
+    best_kp, best_ki, best_kd = 0, 0, 0
+    best_OPT = float('inf')
+
+    for kp in kp_values:
+        for ki in ki_values:
+            for kd in kd_values:
+                OPT = simulate_PID_opt((kp, ki, kd), q , r)
+                if OPT < best_OPT:
+                    best_OPT = OPT
+                    best_kp, best_ki, best_kd = kp, ki, kd
+                    print(f"Nowe najlepsze nastawy: kp={kp}, ki={ki}, kd={kd} z OPT={OPT}")
+
+    print("\nNajlepsze znalezione nastawy PID:")
+    print(f"kp = {best_kp}, ki = {best_ki}, kd = {best_kd}")
+    print(f"Minimalny wskaźnik OPT = {best_OPT}")
+
+    return best_kp, best_ki, best_kd, best_OPT
+
+
 def zad1():
     g_kp = 75.5
     g_T = 1.61
@@ -125,4 +173,15 @@ def zad1():
     # IAE = 1.3
     # ITAE = 2.11
     plot_PID(calc_PID(g_kp, g_T))
-optimize_PID()
+def zad3():
+    g_kp = 75.5
+    g_T = 1.61
+    # Dla poniższych q i r najlepsze nastawy to 0,0,0 - najbardziej opłaca się nic nie robić XD
+    q = 1
+    r = 1
+    # Dla poniższych q i r optymalne nastawy to ok: kp=10.44, ki=0.21, kd=6.21, opt=4.17
+    q = 1
+    r = 0.01
+    optimize_PID_opt(q, r)
+    # print(simulate_PID_opt(calc_PID(g_kp, g_T), q, r))
+zad3()
